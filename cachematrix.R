4eaa7eb00892fe6d964f303919184647f5fcb4ea
sona99 <sonja.hecht@web.de>
2016-07-15 09:49:25 +0200

    this is my function for assignment 2
    Signed-off-by: sona99 <sonja.hecht@web.de>

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function creates a special "matrix" object that can cache its inverse.
 
-## Write a short comment describing this function
+makeCacheMatrix <- function(inp_matrix = matrix()) {
 
-makeCacheMatrix <- function(x = matrix()) {
+## initialize the inverse matrix         
+    inverse_matrix <- NULL
+
+## store input matrix in seperate environment    
+        set <- function(input_matrix_new) {
+        inp_matrix <<- input_matrix_new
+        inverse_matrix <<- NULL
+        }
+## get the stored matrix from the seperate environment        
+    get <- function() inp_matrix
+
+## store the inverse of the matrix in seperate environment    
+    setinvmatrix <- function(inv_matrix) inverse_matrix <<- inv_matrix
+
+## get the inverse of the matrix from the seperate environment        
+    getinvmatrix <- function() inverse_matrix
+
+## print functions     
+    list(set = set, get = get,
+         setinvmatrix = setinvmatrix,
+         getinvmatrix = getinvmatrix)    
+    
+    
 
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the "special matrix" returned by `makeCacheMatrix` above. 
+## If the inverse has already been calculated (and the matrix has not changed), then
+## cacheSolve will retrieve the inverse from the cache.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+cacheSolve <- function(special_matrix, ...) {
+    
+
+## check if inverse matrixed is cached an if yes, return its value    
+    cached_invmatrix <- special_matrix$getinvmatrix()
+    if(!is.null(cached_invmatrix)) {
+        message("getting cached data")
+        return(cached_invmatrix)
+    }
+## otherwise, the inverse matrix will be calculated, cached and returend    
+    ## get input matrix from seperate environment    
+    data <- special_matrix$get()
+    ## calculate inverse matrix    
+    cached_invmatrix <- solve(data, ...)
+    ## cache inverse matrix
+    special_matrix$setinvmatrix(cached_invmatrix)
+    ## return inverse matrix
+    cached_invmatrix
+    
 }
